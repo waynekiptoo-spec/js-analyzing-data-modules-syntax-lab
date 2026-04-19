17f5ece84087c7ef2fd7ca0f084ae14015079881
wayne kiptoo <waynekiptoo26@gmail.com>
2026-04-19 08:40:11 +0300

    Submission

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+require("datejs");
 
+function combineUsers(...args) {
+    const combinedObject = {
+        users: []
+    };
 
+    
+    for (let arr of args) {
+        combinedObject.users.push(...arr);
+    }
 
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+    
+    combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+    return combinedObject;
+}
+
+module.exports = combineUsers;
